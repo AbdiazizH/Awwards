9a55bcd3a38be6119c7c6fe8511474fbce31c61a
AbdiazizH <a.aziz69890@gmail.com>
2021-07-27 19:53:40 +0300

    changed uploader to charField

--- projects/views.py
+++ projects/views.py
@@ -15,7 +15,6 @@ from .serializer import ProfileSerializer,ProjectSerializer
 
 # Create your views here.
 
-
 def index(request):
     projects = Projects.objects.all()
     return render(request,'index.html',{"projects":projects})
