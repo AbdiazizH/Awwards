273fb0a66126b66f6f24029d45ac0a8377011824
AbdiazizH <a.aziz69890@gmail.com>
2021-07-27 19:45:03 +0300

    added cloudinary

--- projects/views.py
+++ projects/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 
+
 from django.contrib.auth import authenticate, login
 from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required
